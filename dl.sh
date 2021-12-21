06a4672ea377d3bb2702e7e904d7035ecf9b946e
Ryan Plyler <g.r.plyler@gmail.com>
2021-12-21 01:51:51 -0500

    dl

--- dl.sh
+++ dl.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+BTAR=blender-3.0.0-linux-x64.tar.xz
+if [ -f "$BTAR" ]; then
+    echo "$BTAR already download exists."
+    exit
+fi
+
 wget https://mirror.clarkson.edu/blender/release/Blender3.0/blender-3.0.0-linux-x64.tar.xz
 tar xvf blender-3.0.0-linux-x64.tar.xz
+mv blender-3.0.0-linux-x64 blender
+cd blender
+ln -s `pwd`/blender /usr/local/bin/blender
+cd ..
+echo "blender 3.0.0 installed"
