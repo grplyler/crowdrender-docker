3f463616189195ba83e7fbaf793ac550c0242a96
Ryan Plyler <g.r.plyler@gmail.com>
2021-12-21 01:55:44 -0500

    cr dl

--- dl.sh
+++ dl.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
 BTAR=blender-3.0.0-linux-x64.tar.xz
+CRZIP=cr_031_bl280.zip
+CRDL=https://discovery.crowd-render.com/api/v0/download-addon/$CRZIP
+
+# Check for blender
 if [ -f "$BTAR" ]; then
-    echo "$BTAR already download exists."
-    exit
+    echo "$BTAR already downloaded."
+else
+    # Download Blender
+    wget https://mirror.clarkson.edu/blender/release/Blender3.0/blender-3.0.0-linux-x64.tar.xz
+    tar xvf blender-3.0.0-linux-x64.tar.xz
+    mv blender-3.0.0-linux-x64 blender
+    cd blender
+    ln -s `pwd`/blender /usr/local/bin/blender
+    cd ..
+    echo "blender 3.0.0 installed"
 fi
 
-wget https://mirror.clarkson.edu/blender/release/Blender3.0/blender-3.0.0-linux-x64.tar.xz
-tar xvf blender-3.0.0-linux-x64.tar.xz
-mv blender-3.0.0-linux-x64 blender
-cd blender
-ln -s `pwd`/blender /usr/local/bin/blender
-cd ..
-echo "blender 3.0.0 installed"
+# Check for crowdrender
+if [ -f "$BTAR" ]; then
+    echo "$CRZIP already downloaded."
+else
+    echo "downloading crowdrender..."
+    wget $CRDL
+fi
